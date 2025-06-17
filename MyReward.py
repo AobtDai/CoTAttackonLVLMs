359c06920141ee625935e86a2a2e0b182ab0880f
AobtDai <adai590@connect.hkust-gz.edu.cn>
2025-06-17 09:54:28 +0800

    fillin

--- MyReward.py
+++ MyReward.py
@@ -3,6 +3,9 @@ import torch
 from datasets import load_dataset, Dataset
 from typing import Union
 
+from Eval import get_llm_response
+from Output_Extract import analyze_sections
+
 class MyReward:
     def __init__(self):
         pass
@@ -29,23 +32,30 @@ class MyReward:
                 score[i] = (leng_i - 100) * 2.0 / 1000
         return score
 
-    def get_context_aware_score(self, system_prompt, gen_answer):
-        return 0
+    def get_context_aware_score(self, system_prompt, completions, gen_answer, **kwargs) -> list[float]:
+        responses = [completion[0]['content'] for completion in completions]
+        raw_llm_eval = [get_llm_response(r) for r in responses]
+        extracted_scores = [analyze_sections(raw_e) for raw_e in raw_llm_eval]
+        return [0. if scores[1]==-1 else scores[1]*0.9 + scores[2]*0.5 for scores in extracted_scores]
+        # return [0.5 if r.isdigit() else 0.0 for r in extracted_responses]
 
-    def context_aware_reward_func(self, prompts, completions, gen_answer, **kwargs) -> list[float]:
-        system_prompt_context_aware = '''
-        Divide the answer into several part. Then summarize each part and evaluate the correlation between each adjacent parts
-        ...
-        '''
-        # Send system_prompt & gen_answer to eval model
-        return [self.get_context_aware_score(system_prompt_context_aware, t) for t in gen_answer]
+    # def context_aware_reward_func(self, prompts, completions, gen_answer, **kwargs) -> list[float]:
+    #     system_prompt_context_aware = '''
+    #     Divide the answer into several part. Then summarize each part and evaluate the correlation between each adjacent parts
+    #     ...
+    #     '''
+    #     # Send system_prompt & gen_answer to eval model
+    #     return [self.get_context_aware_score(system_prompt_context_aware, t) for t in gen_answer]
 
     def semantic_reward_func(self, prompts, completions, gen_answer, **kwargs) -> list[float]:
+        # 定义语义奖励函数，输入为prompts、completions、gen_answer和kwargs，输出为列表[float]
         system_prompt_context_aware = '''
         Check the grammar of this text. And analyze the level of its semantic correlation.
         ...
         '''
+        # 定义系统提示，用于评估文本的语法和语义相关性
         # Send system_prompt & gen_answer to eval model
+        # 将系统提示和生成答案发送到评估模型
         return [self.get_context_aware_score(system_prompt_context_aware, t) for t in gen_answer]
 
 
